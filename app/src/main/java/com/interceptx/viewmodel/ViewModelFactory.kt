6d60066a0fcc29a1221c767707d4cbc3caaa836f
Elmagicmark <121581683+Elmagicmark@users.noreply.github.com>
2026-07-28 09:06:42 +0300

    Update ViewModelFactory to use application context

--- app/src/main/java/com/interceptx/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/interceptx/viewmodel/ViewModelFactory.kt
@@ -7,8 +7,8 @@ import com.interceptx.InterceptXApp
 class ViewModelFactory(private val app: InterceptXApp) : ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T = when (modelClass) {
-        DashboardViewModel::class.java -> DashboardViewModel(app.repository, app.proxyEngine) as T
-        InterceptViewModel::class.java -> InterceptViewModel(app.repository, app.interceptQueue) as T
+        DashboardViewModel::class.java -> DashboardViewModel(app.applicationContext, app.repository, app.proxyEngine) as T
+        InterceptViewModel::class.java -> InterceptViewModel(app.repository, app.interceptQueue, app.proxyEngine) as T
         HistoryViewModel::class.java -> HistoryViewModel(app.repository) as T
         RepeaterViewModel::class.java -> RepeaterViewModel(app.repository) as T
         CertificatesViewModel::class.java -> CertificatesViewModel(app.certificateAuthority) as T
